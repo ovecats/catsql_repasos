d7a34a15a11ac8bf250cceada0b9000f86acf3e0
ovecats <datc23@gmail.com>
2024-03-16 22:01:23 -0500

    actualizar1603

---
+++ soluciones160324.sql
@@ -0,0 +1,18 @@
+USE facultad;
+
+-- 1) Caracter, nº de alumnos, nota mínima, máxima y media de las asignaturas.
+-- Ordenar el resultado por curso primero y nombre de la asignatura después.
+select caracter, 
+
+-- 2) Asignaturas con una nota media inferior a 5 orden por curso y posteriormente por nombre ascendente
+
+
+
+-- 3) Conocer el número de profesores por categoría  y ordenar la tabla de categoría con más profesores a categoría con menos
+
+
+-- 4) Conocer curso, asignatura, caracter de la asignatura, coordinador y e-mail de contacto
+
+
+
+-- 5) Asignaturas impartidas por profesor de más a menos
