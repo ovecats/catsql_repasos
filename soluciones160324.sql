3efe60406158eb1e6f0a076c8bc03e528980c736
ovecats <datc23@gmail.com>
2024-03-18 22:11:50 -0500

    180324

--- soluciones160324.sql
+++ soluciones160324.sql
@@ -30,3 +30,36 @@ ORDER BY curso ASC, asignatura ASC;
 
 
 -- 5) Asignaturas impartidas por profesor de más a menos
+
+ SELECT concat(profesor.nombre, ' ', profesor.apellido1, ' ', profesor.apellido2) AS Profesor, count(impartir.idAsignatura) AS materiasImpartidas
+ FROM profesor INNER JOIN impartir ON profesor.idProfesor = impartir.idProfesor 
+ GROUP BY impartir.idProfesor
+ ORDER BY materiasImpartidas DESC;
+ 
+ 
+ -- 6) Mostrar aquellos alumnos que tienen una media superior a 7.00 y su nota media
+  SELECT concat(nombre, ' ', apellido1, ' ', apellido2) AS alumno, round(avg(matricula.nota),2) AS notaMedia
+FROM alumno INNER JOIN matricula ON alumno.idAlumno = matricula.idAlumno
+GROUP BY alumno.idAlumno
+HAVING avg(matricula.nota) > 7.00 -- Filto de la agrupación alumno con una nota media superior a 7
+ORDER BY notaMedia DESC;
+ 
+ -- 7) Obtener los créditos totales por curso (con separación según caracter). El curso 6 tiene 0 créditos, al ser un doctorado las asignaturas no forman parte de la nota final
+ 
+ -- 8) Obtener aquellas optativas sin alumnos
+ 
+ -- 9) Obtener el número de alumnos de primero que tienen que recuperar cada asignatura
+ 
+ -- 10) Alumnos que tienen que presentarse a la recuperación de Algebra lineal y la nota que sacaron 
+ 
+ -- 11) Alumnos de segundo que han sacado un 10 en alguna materias para ponerles mención de honor. Ordenar alfabéticamente por nombre asignatura 
+ 
+ -- 12) Conocer número de alumnos totales, los becados y porcentaje de becados respecto al total
+ 
+ -- 13) Conocer nota media de los becados y el curso en el que están. Ordenar resultado por nombre completo descendiente
+ 
+  -- 14) Queremos repartir un bonus a los 10 profesores con mejores medias en las asignaturas que imparten
+  
+  -- 15) Obtener toda la información de asignaturas que contienen la palabla 'datos' o 'progra' en su nombre. Ordenar por curso y luego por nombre
+  
+  -- 16) Obtener listado para enviar un comunicado oficial personalizado con el nombre a todas las personas de la universidad (alumnos y profesores)
